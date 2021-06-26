9b26927a9f36e8d55f0e1dcbbe676f3fc19760a3
PythonKid <dprieto93@hotmail.com>
2021-06-26 19:08:56 +0200

    [WIP] Configuration of Firebase Store and Storage performed. Development of login interface design and logic using Firebase Auth. Hardcoded test user on Firebase Auth.

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -3,15 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginViewComponent } from './login-view/login-view.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-
+// Módulo para componentes Material
+import { MaterialDesignModule } from '../../modules/material-design/material-design.module';
 @NgModule({
   declarations: [
     LoginViewComponent
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    MaterialDesignModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class LoginModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBUKtstduEJu1iM2efDuUV0lBpv6QMrtCQ",
+    authDomain: "chatitgood.firebaseapp.com",
+    projectId: "chatitgood",
+    storageBucket: "chatitgood.appspot.com",
+    messagingSenderId: "516541012128",
+    appId: "1:516541012128:web:cc7f78304b2381558711c5"
+  }
 };
 
 /*
